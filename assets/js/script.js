b36ddd98acca2211a93d63ea06aa00aeb83288c5
hannahschwen <hannahschwen@gmail.com>
2024-05-10 13:54:58 -0400

    added function to delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
+/*
+Where I'm at:
+
+Crete task card is functioning, currently using test text. Need to replace it with form input.
+
+Delete button works.
+*/
+
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+const body = $('body');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -11,12 +22,12 @@ function generateTaskId() {
 function createTaskCard(task) {
   // Create elements
   const toDoSection = $('#todo-cards');
-  const taskCardEl = $('<div class="card m-4 mt-2">');
+  const taskCardEl = $('<div class="task-card card m-4 mt-2">');
   const taskNameEl = $('<h5 class="card-header">');
   const cardBodyEl = $('<div class="card-body">');
   const taskDescriptionEl = $('<p class="card-text">');
   const taskDateEl = $('<p class="card-text">');
-  const deleteBtnEl = $('<button class="btn btn-danger">Delete</button>');
+  const deleteBtnEl = $('<button class="btn btn-danger delete">Delete</button>');
 
   // Add text
   // Todo: replace this test text with form input
@@ -30,7 +41,7 @@ function createTaskCard(task) {
   cardBodyEl.append(taskDescriptionEl, taskDateEl, deleteBtnEl);
 }
 
-createTaskCard();
+createTaskCard(); // here temporarily to test
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -42,10 +53,14 @@ function handleAddTask(event){
 
 }
 
-// Todo: create a function to handle deleting a task
+// function to handle deleting a task
 function handleDeleteTask(event){
-
+  const btnClicked = $(event.target);
+  btnClicked.parents('.task-card').remove();
 }
+
+// uses delegation on the body to listen for a click on element with class of delete
+body.on('click', '.delete', handleDeleteTask);
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
