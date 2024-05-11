0315ee8bd66def89fd364458efb95307adbba740
hannahschwen <hannahschwen@gmail.com>
2024-05-11 10:28:20 -0400

    reverted to repair logic, wrote new createTaskCard and renderTaskList functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,47 @@
-// Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+/*
+PSEUDO CODE:
 
-const body = $('body');
-const taskTitleInput = $('#task-title');
-const taskDescriptionInput = $('#task-description');
-const taskDateInput = $('#task-date');
+After the page loads:
+  - task cards should render sorted according to status
+If a user adds a task:
+  - a unique id is generated for the task
+  - the task is assigned a to-do status
+  - the task is saved to local storage
+  - the task is added to the to-do lane
+If a user drags the task:
+  - the position should update to append or snap in line with the parent lane
+  If the task is dropped in the progress lane
+    - the status should be updated to in-progress
+    - the new status should be updated in local storage
+  IF the task is dropped in the done lane
+    - the status should be updated to done
+    - any bg-warning text-white border border-white classes should be removed
+    - the new status should be updated to done
+If a user clicks the delete button
+  - the associated task of that id should be deleted from local storage
+  - the task card element should also be deleted
+
+*/
+
+
+
+
+// Retrieve tasks and nextId from localStorage
+// let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-  let task = {
-    id : $.now(),
-    title : taskTitleInput.val(),
-    description : taskDescriptionInput.val(),
-    date : taskDateInput.val(),
-  }
 
-  taskList.push(task);
-  localStorage.setItem('tasks', JSON.stringify(taskList));
-
-  console.log(taskList);
 }
 
 // Todo: create a function to create a task card
+// This function makes a task card for a task and sorts it into the right column according to its status.
+// I can probably write this shorter, sine the only thing that's changing is where the card appends to, but look into that later.
 function createTaskCard(task) {
-  // I think this can be to make a task card appear after making a new one, the render task list loads previously saved tasks on page load?
-  // Also, rewrite this more concisely, like I did on the renderTaskList
-  // Create elements
-  const toDoSection = $('#todo-cards');
-  const taskCardEl = $('<div class="task-card card m-4 mt-2">');
-  const taskTitleEl = $('<h5 class="card-header">');
-  const cardBodyEl = $('<div class="card-body">');
-  const taskDescriptionEl = $('<p class="card-text">');
-  const taskDateEl = $('<p class="card-text">');
-  const deleteBtnEl = $('<button class="btn btn-danger delete">Delete</button>');
-
-  // Add text
-  // Todo: replace this test text with form input
-  taskTitleEl.text(task.title);
-  taskDescriptionEl.text(task.description);
-  taskDateEl.text(task.date);
-
-  // Print to To Do section
-  toDoSection.prepend(taskCardEl);
-  taskCardEl.append(taskTitleEl, cardBodyEl);
-  cardBodyEl.append(taskDescriptionEl, taskDateEl, deleteBtnEl);
-}
-
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-  taskList.forEach(task => {
-    $('#todo-cards').prepend(`
-      <div class = "task-card card m-4 mt-2 z-3" id=${task.id}>
+  if (task.status == "done") {
+    $('#done-cards').append(`
+      <div class = "task-card card m-4 mt-2" id=${task.id}>
         <h5 class="card-header">${task.title}</h5>
         <div class="card-body">
           <p class="card-text">${task.description}</p>
@@ -60,15 +50,49 @@ function renderTaskList() {
         </div>
       </div>
     `);
+  } else if (task.status == "in-progress") {
+    $('#in-progress-cards').append(`
+    <div class = "task-card card m-4 mt-2" id=${task.id}>
+      <h5 class="card-header">${task.title}</h5>
+      <div class="card-body">
+        <p class="card-text">${task.description}</p>
+        <p class="card-text">${task.date}</p>
+        <button class="btn btn-danger delete">Delete</button>
+      </div>
+    </div>
+  `);
+  } else {
+    $('#todo-cards').append(`
+    <div class = "task-card card m-4 mt-2" id=${task.id}>
+      <h5 class="card-header">${task.title}</h5>
+      <div class="card-body">
+        <p class="card-text">${task.description}</p>
+        <p class="card-text">${task.date}</p>
+        <button class="btn btn-danger delete">Delete</button>
+      </div>
+    </div>
+  `);
+  }
+}
+
+// This is an example task list so I could test the renderTaskList function.
+const taskList = [{title : 'To do', description: 'test description', date: 'Test date', status: 'todo',}, {title : 'In progress', description: 'test description', date: 'Test date', status: 'in-progress',}, {title : 'Done', description: 'test description', date: 'Test date', status: 'done',},]
+
+// Todo: create a function to render the task list and make cards draggable
+// This will run the createTaskCard function for every task in the taskList. This function will run on page load
+function renderTaskList() {
+  taskList.forEach(task => {
+    createTaskCard(task);
   });
 
-    $('.task-card').draggable({
-      cursor: 'move',
-      revert: "invalid",
-      zIndex: 100,
-      // I need to figure out how to get the items to 'make room' for the dropped element and snap in place.. handleDrop function
-    });
-  };
+  // This makes each card draggable
+  $('.task-card').draggable({
+    revert: "invalid",
+  })
+
+}
+
+renderTaskList();
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
@@ -77,13 +101,8 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-  const btnClicked = $(event.target);
-  btnClicked.parents('.task-card').remove();
-  // this deletes the task card element, but not from local storage
-}
 
-// listen for a click on element with class of delete
-body.on('click', '.delete', handleDeleteTask);
+}
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
@@ -92,22 +111,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-  renderTaskList();
-
-
-  const submitTask = $('#submit-task');
-
-  submitTask.on('click', function (event) {
-    event.preventDefault();
-    generateTaskId();
-    createTaskCard();
-  })
-
-  // listen for a click on element with class of delete
-  body.on('click', '.delete', handleDeleteTask);
-
-  $('.lane').droppable({
-    //I need to figure out how to get the dropped elements to snap in place.. Prob a handleDrop function!
-  });
 
 });
